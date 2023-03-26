6ebec13fe10f6f2b90693d082eb5c4ab0ec5d834
Priyaunnikrishnan <99670621+Priyaunnikrishnan@users.noreply.github.com>
2023-03-26 12:17:54 +0530

    Creating admin credentials

--- projects/app1/admin.py
+++ projects/app1/admin.py
@@ -1,3 +1,13 @@
+from .models import Form
 from django.contrib import admin
 
-# Register your models here.
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("date", "occupation")
+    ordering = ("first_name",)
+    readonly_fields = ("occupation",)
+
+
+admin.site.register(Form, FormAdmin)
